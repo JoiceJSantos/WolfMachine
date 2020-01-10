8bf5b902922a93371ef1e5878492518c684b8c26
JOICE JUSSARA SANTOS CESÁRIO <56134122+JoiceJSantos@users.noreply.github.com>
2020-01-10 08:56:00 -0300

    Site Wolf

--- contato/contato.php
+++ contato/contato.php
@@ -7,16 +7,16 @@ $nome = $_REQUEST['nome'];
 $email = $_REQUEST['email'];
 $msg = $_REQUEST['mensagem'];
 
-        $corpo = "<strong>Mensagem de contato</strong><br><br>";
-        $corpo .= "<strong>Nome: $nome</strong><br>";
-        $corpo .= "<strong>E-mail: $email</strong><br>";
-        $corpo .= "<strong>MSG:$msg</strong><br>";
+$corpo = "<strong>Mensagem de contato</strong><br><br>";
+$corpo .= "<strong>Nome: $nome</strong><br>";
+$corpo .= "<strong>E-mail: $email</strong><br>";
+$corpo .= "<strong>MSG:$msg</strong><br>";
 
-        $header = "Content-type: text/html; charset: utf-8\n";
-        $header .= "From: $email Reply-to $email\n";
+$header = "Content-type: text/html; charset: utf-8\n";
+$header .= "From: $email Reply-to $email\n";
 
 
- @mail($para, $assunto,$corpo,$header);
- header("location:faleconosco.php?msg%20=%20enviado");
+@mail($para, $assunto,$corpo,$header);
+header("location:faleconosco.php?msg%20=%20enviado");
 
 ?>
